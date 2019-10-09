0dec16bbb8e6351a0d8dd459ceb65436e29a9217
Igor Aleksanov <popzxc@yandex.ru>
2019-10-09 17:19:55 +0300

    Add more tests

---
+++ tests/spec_loaders.py
@@ -0,0 +1,25 @@
+# pylint: disable=missing-docstring, protected-access
+import unittest
+from typing import Dict, Any
+
+from exonum_client.protobuf_loader import ProtobufLoader
+from exonum_launcher.configuration import Instance
+from exonum_launcher.runtimes.runtime import RuntimeSpecLoader
+from exonum_launcher.instances.instance_spec_loader import InstanceSpecLoader
+
+
+class TestRuntimeSpecLoader(RuntimeSpecLoader, unittest.TestCase):
+    """Artifact spec encoder for Rust runtime"""
+
+    def encode_spec(self, data: Dict[str, Any]) -> bytes:
+        self.assertTrue("parameter" in data)
+        self.assertEqual(data["parameter"], "value")
+        return b"runtime_result"
+
+
+class TestInstanceSpecLoader(InstanceSpecLoader, unittest.TestCase):
+    def load_spec(self, loader: ProtobufLoader, instance: Instance) -> bytes:
+        self.assertEqual(instance.artifact.runtime, "sample")
+        self.assertEqual(instance.artifact.name, "cryptocurrency")
+
+        return b"instance_result"
--- tests/test_config.py
+++ tests/test_config.py
@@ -2,19 +2,53 @@
 
 import unittest
 import os
+import copy
 
 from exonum_launcher.configuration import Configuration
 
+_RUNTIMES_START_STATE = copy.deepcopy(Configuration.runtimes())
+_DIR_PATH = os.path.dirname(os.path.realpath(__file__))
+
 
 class TestConfiguration(unittest.TestCase):
-    def test_parse(self) -> None:
-        dir_path = os.path.dirname(os.path.realpath(__file__))
-        config_path = os.path.join(dir_path, "sample_config.yml")
-        config = Configuration.from_yaml(config_path)
+    @staticmethod
+    def load_config(file_name: str) -> Configuration:
+        """Loads Configuration from the sample .yml file"""
+        config_path = os.path.join(_DIR_PATH, "test_data", file_name)
+        return Configuration.from_yaml(config_path)
+
+    def tearDown(self) -> None:
+        # Clean global runtimes state after use.
+        runtimes = Configuration.runtimes()
+        if runtimes != _RUNTIMES_START_STATE:
+            runtimes.clear()
+            for key in _RUNTIMES_START_STATE:
+                runtimes[key] = _RUNTIMES_START_STATE[key]
+
+    def test_defaults(self) -> None:
+        self.assertTrue("rust" in Configuration.runtimes())
+        self.assertEqual(Configuration.runtimes()["rust"], 0)
+
+    def test_declare_runtime(self) -> None:
+        Configuration.declare_runtime("test", 2)
+
+        runtimes = Configuration.runtimes()
+        self.assertTrue("rust" in runtimes)
+        self.assertTrue("test" in runtimes)
+        self.assertEqual(runtimes["rust"], 0)
+        self.assertEqual(runtimes["test"], 2)
+
+    def test_sample_parse(self) -> None:
+        config = self.load_config("sample_config.yml")
 
         self.assertEqual(len(config.networks), 2)
         self.assertEqual(config.networks[0]["host"], "127.0.0.1")
         self.assertEqual(config.networks[1]["host"], "8.8.8.8")
+
+        self.assertTrue("runtime" in config.plugins)
+        self.assertTrue("artifact" in config.plugins)
+        self.assertEqual(len(config.plugins["runtime"]), 0)
+        self.assertEqual(len(config.plugins["artifact"]), 0)
 
         self.assertTrue("cryptocurrency" in config.artifacts)
 
@@ -34,3 +68,27 @@ class TestConfiguration(unittest.TestCase):
             self.assertEqual(instance.name, name)
             self.assertEqual(instance.deadline_height, 10000)
             self.assertIsNone(instance.config)
+
+    def test_parse_runtimes(self) -> None:
+        config = self.load_config("custom_runtimes.yml")
+
+        runtimes = config.runtimes()
+        expected_layout = {"rust": 0, "example": 2, "other_example": 42}
+        self.assertEqual(runtimes, expected_layout)
+
+        self.assertTrue("cryptocurrency" in config.artifacts)
+        self.assertTrue("other_cryptocurrency" in config.artifacts)
+        self.assertEqual(config.artifacts["cryptocurrency"].runtime, "example")
+        self.assertEqual(config.artifacts["cryptocurrency"].runtime_id, 2)
+        self.assertEqual(config.artifacts["other_cryptocurrency"].runtime, "other_example")
+        self.assertEqual(config.artifacts["other_cryptocurrency"].runtime_id, 42)
+
+    def test_parse_plugins(self) -> None:
+        config = self.load_config("custom_plugins.yml")
+
+        expected_layout = {
+            "runtime": {"sample": ".spec_loaders.TestRuntimeSpecLoader"},
+            "artifact": {"cryptocurrency": ".spec_loaders.TestInstanceSpecLoader"},
+        }
+
+        self.assertEqual(config.plugins, expected_layout)
